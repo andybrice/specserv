d1d6289a95679d73c61a7f6f1b50cfe8c4f19a5f
Andy Brice <andybrice@andybrice.default>
2014-07-27 23:08:27 +0100

    Added TODO about RSpec executable

--- lib/specserv.rb
+++ lib/specserv.rb
@@ -6,7 +6,7 @@ class MalformedResultsError < StandardError; end
 
 class RSpecGateway
   def run( source_path )
-    `rspec --format html #{source_path}`
+    `rspec --format html #{source_path}` # TODO run RSpec through RSpec::Core::Runner rather than through executable
   end
 end
 
